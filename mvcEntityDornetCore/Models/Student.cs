d04c8560ed7bcf9cda3e6b90ee808cc6b8575812
pritpalkaur05@gmail.com <pritpalkaur05@gmail.com>
2020-05-26 10:47:22 +0800

    dotnet core MVC

---
+++ mvcEntityDornetCore/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcEntityDornetCore.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MidNames { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
